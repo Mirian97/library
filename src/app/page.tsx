3a926d2150cefb55c573899334b234917ce78817
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-08-01 18:20:26 -0300

    feat: added book item component in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <main> Página home</main>
+import BookItem from '@/components/BookItem'
+
+const Home = () => {
+  return (
+    <div className='pt-3'>
+      <h2 className='font-bold text-3xl text-primary-500 text-center'>Seção de livros</h2>
+      <div className='flex flex-wrap justify-evenly gap-6 mt-5'>
+        <BookItem />
+      </div>
+    </div>
+  )
 }
+
+export default Home
