f7462dde2f2a42fc37ac8bf95018dcce137004ca
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-08-01 19:26:59 -0300

    feat: added animation text-exapnd to title

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,23 @@ module.exports = {
       },
       boxShadow: {
         100: '0 8px 14px 0 rgba(0, 0, 0, 0.2)'
+      },
+      animation: {
+        'text-expand': 'text-expand .8s cubic-bezier(0.215, 0.61, 0.355, 1) both'
+      },
+      keyframes: {
+        'text-expand': {
+          '0%': {
+            letterSpacing: '-1rem',
+            opacity: 0
+          },
+          '50%': {
+            opacity: 0.5
+          },
+          '100%': {
+            opacity: 1
+          }
+        }
       }
     }
   },
