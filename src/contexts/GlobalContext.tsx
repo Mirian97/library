b6cc15b651f649e5292528e14bb2be5fc4243e04
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-08-02 09:21:33 -0300

    feat: added new states and functions in global context

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -1,11 +1,15 @@
 'use client'
 import useGlobalProvider from '@/hooks/useGlobalProvider'
+import IBook from '@/interfaces/IBook'
 import IChildren from '@/interfaces/IChildren'
 import { createContext } from 'react'
 
 interface IGlobalContextData {
   showSearchInput: boolean
   toggleSearchInput: () => void
+  books: IBook
+  isLoadingBooks: boolean
+  handleGetOneBook: (bookId: string) => Promise<void>
 }
 
 const GlobalContext = createContext<IGlobalContextData>({} as IGlobalContextData)
