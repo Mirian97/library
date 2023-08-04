b46ba320aa85d1ca6e74a39a5da3ac7a43632cbf
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-08-04 10:49:21 -0300

    feat: added a padding in wrap title  and authors

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -15,10 +15,12 @@ const BookItem = ({ id, volumeInfo }: IBookItem) => {
             className='w-full h-full rounded-t-2xl bg-cover'
             style={{ backgroundImage: `url(${bookImage})` }}
           />
-          <p className='text-sm'>Título: {formatString(title, 80)}</p>
-          {authors && (
-            <p className='text-sm'>Autor(es): {formatArrayToString(authors, 40)}</p>
-          )}
+          <div className='py-2'>
+            {title && <p className='text-sm'>Título: {formatString(title, 35)}</p>}
+            {authors && (
+              <p className='text-sm'>Autor(es): {formatArrayToString(authors, 35)}</p>
+            )}
+          </div>
         </div>
         <div className='book-item-back'>
           <p className='text-lg font-bold'>Veja mais</p>
