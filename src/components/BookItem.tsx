3c6aff9aca8dd7ee51e19f1643ff4662c12ddbc4
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-08-04 09:43:48 -0300

    fix: book item component

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,23 +1,24 @@
 import IBookItem from '@/interfaces/IBookItem'
+import { formatArrayToString } from '@/utils/formatters'
+import { getBookImage } from '@/utils/functions'
 import Link from 'next/link'
 import { memo } from 'react'
 
 const BookItem = ({ id, volumeInfo }: IBookItem) => {
-  const {
-    title,
-    authors,
-    imageLinks: { thumbnail }
-  } = volumeInfo
+  const { title, authors, imageLinks } = volumeInfo
+  const bookImage = getBookImage(imageLinks)
   return (
     <Link href={`livros/${id}`} className='book-item'>
       <div className='book-item-wrap'>
         <div className='book-item-front'>
           <div
             className='w-full h-full rounded-t-2xl bg-cover'
-            style={{ backgroundImage: `url(${thumbnail})` }}
+            style={{ backgroundImage: `url(${bookImage})` }}
           />
           <p className='text-sm'>Título: {title}</p>
-          <p className='text-sm'>Autor(es): {authors?.join(', ')}</p>
+          {authors && (
+            <p className='text-sm'>Autor(es): {formatArrayToString(authors, 40)}</p>
+          )}
         </div>
         <div className='book-item-back'>
           <p className='text-lg font-bold'>Veja mais</p>
