569b2869d4634db2bcd5c2f215196373c8b19df0
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-08-04 08:59:42 -0300

    fix: modifications in functions formatters

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,10 +1,12 @@
 export const removeHtmlTags = (children: React.ReactNode) =>
+  children &&
   children!
     .toString()
     .replace(/(<([^>]+)>)/gi, '')
     .replace(/&nbsp;/gi, ' ')
 
 export const formatDate = (day: string) => {
+  if (!day) return
   const options: Intl.DateTimeFormatOptions = {
     day: '2-digit',
     month: '2-digit',
@@ -16,6 +18,12 @@ export const formatDate = (day: string) => {
 }
 
 export const formatBRLMoney = (value: number) =>
-  value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+  value && value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
 
-export const formatArrayToString = (array?: string[]) => array?.join(', ')
+export const formatArrayToString = (array: string[], maxLength?: number) => {
+  const string = array.join(', ')
+  if (maxLength && string.length >= maxLength) {
+    return `${string.slice(0, maxLength)}...`
+  }
+  return string
+}
