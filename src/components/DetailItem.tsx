aac7a293c0cb6bab2d87626d3df49555246eb778
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-08-04 09:00:29 -0300

    fix: added class hidden if not exists description in detail

--- src/components/DetailItem.tsx
+++ src/components/DetailItem.tsx
@@ -6,7 +6,7 @@ interface IDetailItem {
 
 const DetailItem = ({ name, description }: IDetailItem) => {
   return (
-    <div className='text-justify'>
+    <div className={`text-justify ${!description && 'hidden'}`}>
       <span className='text-primary-800 font-bold'>{name}: </span>
       <span className='text-gray-600'>{description}</span>
     </div>
