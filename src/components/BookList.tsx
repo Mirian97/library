432078369cc02b0d8dded24b5a51c70bf3cea056
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-08-04 09:42:31 -0300

    fix: added search with debounced and message if not exists any book with the search term

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -2,21 +2,19 @@
 'use client'
 import { defaultBooks } from '@/constants/book'
 import useDebounce from '@/hooks/useDebounce'
+import useGlobal from '@/hooks/useGlobal'
 import IBook from '@/interfaces/IBook'
 import { getBooks } from '@/services/book'
 import { messageError } from '@/utils/toast'
 import Image from 'next/image'
-import { ChangeEvent, memo, useEffect, useState } from 'react'
+import { memo, useEffect, useState } from 'react'
 import BookItem from './BookItem'
-import Input from './Input'
 
 const BookList = () => {
-  const [searchInputValue, setSearchInputValue] = useState('')
-  const [books, setBooks] = useState<IBook>({ ...defaultBooks })
+  const { searchInputValue } = useGlobal()
+  const searchDebounced = useDebounce(searchInputValue)
+  const [books, setBooks] = useState<IBook>(defaultBooks)
   const [isLoadingBooks, setIsLoadingBooks] = useState(true)
-  const searchDebounced = useDebounce(searchInputValue, 700)
-  const onChangeSearchInput = (e: ChangeEvent<HTMLInputElement>) =>
-    setSearchInputValue(e.target.value)
 
   const handleGetBooks = async () => {
     setIsLoadingBooks(true)
@@ -29,24 +27,13 @@ const BookList = () => {
       setIsLoadingBooks(false)
     }
   }
+
   useEffect(() => {
-    if (searchDebounced) {
-      handleGetBooks()
-      console.log('ola com debounce')
-    } else {
-      handleGetBooks()
-      console.log('ola normal')
-    }
+    searchDebounced ? handleGetBooks() : handleGetBooks()
   }, [searchDebounced])
 
   return (
     <div className='flex flex-col items-center mt-4'>
-      <Input
-        id='search'
-        type='search'
-        placeholder='Buscar por livro...'
-        onChange={onChangeSearchInput}
-      />
       <div className='flex flex-wrap justify-evenly gap-6 mt-5'>
         {isLoadingBooks ? (
           <Image
@@ -56,8 +43,10 @@ const BookList = () => {
             height={80}
             className='mt-5'
           />
+        ) : books.totalItems ? (
+          books.items.map((book) => <BookItem key={book.id} {...book} />)
         ) : (
-          books.items?.map((book) => <BookItem key={book.id} {...book} />)
+          <h5 className='text-lg'>Não há livros para o termo pesquisado</h5>
         )}
       </div>
     </div>
