53e60121df7cf2f81d87143fb40c050ab79a43ff
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-08-09 16:37:52 -0300

    style: removed return in detail item component

--- src/components/DetailItem.tsx
+++ src/components/DetailItem.tsx
@@ -4,13 +4,11 @@ interface IDetailItem {
   description?: string
 }
 
-const DetailItem = ({ name, description }: IDetailItem) => {
-  return (
-    <div className={`text-justify ${!description && 'hidden'}`}>
-      <span className='text-primary-800 font-bold'>{name}: </span>
-      <span className='text-gray-600'>{description}</span>
-    </div>
-  )
-}
+const DetailItem = ({ name, description }: IDetailItem) => (
+  <div className={`text-justify ${!description && 'hidden'}`}>
+    <span className='text-primary-800 font-bold'>{name}: </span>
+    <span className='text-gray-600'>{description}</span>
+  </div>
+)
 
 export default DetailItem
