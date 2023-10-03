5458de3edd16d36d6647e099614ff79a3ddf1e3f
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-10-03 20:29:06 -0300

    feat: improve tests to loading component

--- src/components/Loading/Loading.test.tsx
+++ src/components/Loading/Loading.test.tsx
@@ -2,16 +2,23 @@ import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 import Loading from './Loading'
 
-const renderLoading = () => render(<Loading />)
+const renderLoading = () => render(<Loading className='custom-class' />)
 
 describe('Loading', () => {
-  it('should return a string', () => {
+  it('should render loading image correctly', () => {
     const { getByAltText } = renderLoading()
-    const loadingImage = getByAltText('Carregando...')
+    const loadingImage = getByAltText(/Carregando.../i)
 
     expect(loadingImage).toBeInTheDocument()
     expect(loadingImage).toHaveAttribute('src')
     expect(loadingImage).toHaveAttribute('width', '80')
     expect(loadingImage).toHaveAttribute('height', '80')
+  })
+
+  it('should pass other HTML attributes', () => {
+    const { getByAltText } = renderLoading()
+    const loadingImage = getByAltText(/Carregando.../i)
+
+    expect(loadingImage).toHaveClass('custom-class', { exact: true })
   })
 })
