fc716e250b8aa32122ab4784ea09f251966d699c
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-10-03 19:22:08 -0300

    feat: create tests to title component

---
+++ src/components/Title/Title.test.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import Title from './Title'
+
+const renderTitle = () => {
+  return render(<Title />)
+}
+describe('Title', () => {
+  it('should render Title correctly', () => {
+    const { getByRole } = renderTitle()
+    const headingElement = getByRole('heading')
+    expect(headingElement).toBeInTheDocument()
+    expect(headingElement).toHaveClass('title')
+  })
+})
