98d563255e08ff9f20804b3e985891d044aef0c5
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-10-07 12:52:15 -0300

    feat: create tests to get book image function

---
+++ src/utils/getBookImage/getBookImage.test.ts
@@ -0,0 +1,28 @@
+import { getBookImage } from './getBookImage'
+
+describe('getBookImage', () => {
+  it('should return the thumbnail URL when imageLinks is provided', () => {
+    const imageLink = {
+      thumbnail: 'https://example.com/book-thumbnail.jpg'
+    }
+    const result = getBookImage(imageLink)
+
+    expect(result).toBe('https://example.com/book-thumbnail.jpg')
+  })
+
+  it('should return the default image URL when imageLinks is undefined', () => {
+    const imageLink = undefined
+    const result = getBookImage(imageLink)
+
+    expect(result).toBe('/image/book-without-cover.jpg')
+  })
+
+  it('should return the default image URL when thumbnail is not provided in imageLinks', () => {
+    const imageLink = {
+      thumbnail: ''
+    }
+    const result = getBookImage(imageLink)
+
+    expect(result).toBe('/image/book-without-cover.jpg')
+  })
+})
