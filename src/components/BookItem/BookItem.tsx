1f49f586ccb23ffa813b296444c0eef054e142ae
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-10-07 09:39:27 -0300

    style: create folder to book item component

---
+++ src/components/BookItem/BookItem.tsx
@@ -0,0 +1,33 @@
+import IBookItem from '@/interfaces/IBookItem'
+import { formatArrayToString, formatString } from '@/utils/formatters'
+import { getBookImage } from '@/utils/functions'
+import Link from 'next/link'
+import { memo } from 'react'
+
+const BookItem = ({ id, volumeInfo }: IBookItem) => {
+  const { title, authors, imageLinks } = volumeInfo
+  const bookImage = getBookImage(imageLinks)
+  return (
+    <Link href={`livros/${id}`} className='book-item'>
+      <div className='book-item-wrap'>
+        <div className='book-item-front'>
+          <div
+            className='w-full h-full rounded-t-2xl bg-cover'
+            style={{ backgroundImage: `url(${bookImage})` }}
+          />
+          <div className='py-2'>
+            {title && <p className='text-sm'>Título: {formatString(title, 35)}</p>}
+            {authors && (
+              <p className='text-sm'>Autor(es): {formatArrayToString(authors, 35)}</p>
+            )}
+          </div>
+        </div>
+        <div className='book-item-back'>
+          <p className='text-lg font-bold'>Veja mais</p>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default memo(BookItem)
