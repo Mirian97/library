7882987d7d140a8de3fb0d4d711d31ddd001c583
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-10-13 18:14:59 -0300

    feat: add condition to verify if maxLength is greater than zero

--- src/utils/formatString/formatString.ts
+++ src/utils/formatString/formatString.ts
@@ -1,5 +1,5 @@
 const formatString = (text: string, maxLength?: number) => {
-  if (maxLength && text.length >= maxLength) {
+  if (maxLength && maxLength > 0 && text.length >= maxLength) {
     return `${text.slice(0, maxLength)}...`
   }
   return text
