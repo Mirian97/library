728a7aea4658141d8ad01c3bd4be1f9ec2893700
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-06 21:40:44 -0300

    feat: add animation in book item

--- src/components/BookItem/BookItem.tsx
+++ src/components/BookItem/BookItem.tsx
@@ -2,14 +2,26 @@ import IBookItem from '@/interfaces/IBookItem'
 import formatArrayToString from '@/utils/formatArrayToString/formatArrayToString'
 import formatString from '@/utils/formatString/formatString'
 import getBookImage from '@/utils/getBookImage/getBookImage'
-import Link from 'next/link'
+import { motion } from 'framer-motion'
+import { useRouter } from 'next/navigation'
 import { memo } from 'react'
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+}
 
 const BookItem = ({ id, volumeInfo }: Pick<IBookItem, 'id' | 'volumeInfo'>) => {
   const { title, authors, imageLinks } = volumeInfo
   const bookImage = getBookImage(imageLinks)
+  const router = useRouter()
+  const navigateToBookItem = () => router.push(`livros/${id}`)
+
   return (
-    <Link href={`livros/${id}`} className='book-item'>
+    <motion.div variants={item} onClick={navigateToBookItem} className='book-item'>
       <div className='book-item-wrap'>
         <div className='book-item-front'>
           <div
@@ -27,7 +39,7 @@ const BookItem = ({ id, volumeInfo }: Pick<IBookItem, 'id' | 'volumeInfo'>) => {
           <p className='text-lg font-bold'>Veja mais</p>
         </div>
       </div>
-    </Link>
+    </motion.div>
   )
 }
 
