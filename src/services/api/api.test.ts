20f68a74284d94afab6cfe204babf1e701806ab8
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-03-03 11:09:19 -0300

    improve test in axios instance

--- src/services/api/api.test.ts
+++ src/services/api/api.test.ts
@@ -1,15 +1,30 @@
+import MockAdapter from 'axios-mock-adapter'
 import api from './api'
 
 describe('Axios Instance Configuration', () => {
-  it('should have baseURL configured correctly', () => {
+  let mock: MockAdapter
+
+  beforeEach(() => {
+    mock = new MockAdapter(api)
+  })
+
+  afterEach(() => {
+    mock.restore()
+  })
+  it('should configured correctly', () => {
     expect(api.defaults.baseURL).toBe('https://www.googleapis.com/books/v1')
-  })
-
-  it('should have timeout configured correctly', () => {
     expect(api.defaults.timeout).toBe(10000)
+    expect(api.defaults.headers['Content-Type']).toBe('application/json')
   })
 
-  it('should have headers with Content-Type configured as application/json', () => {
-    expect(api.defaults.headers['Content-Type']).toBe('application/json')
+  it('should make successfull get request', async () => {
+    const data = {
+      test: 'test'
+    }
+    mock.onGet('/some-endpoint').reply(200, data)
+    const response = await api.get('/some-endpoint')
+
+    expect(response.status).toBe(200)
+    expect(response.data).toEqual(data)
   })
 })
