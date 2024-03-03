6c66302bac1511cd7111a1f1d825f685ffe27f92
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-03-03 17:34:28 -0300

    feat: add data-testid

--- src/app/livros/page.tsx
+++ src/app/livros/page.tsx
@@ -3,7 +3,7 @@ import Title from '@/components/Title/Title'
 import { memo } from 'react'
 
 const Home = () => (
-  <div className='pt-5 pb-10'>
+  <div className='pt-5 pb-10' data-testid='books-page'>
     <Title>Seção de livros</Title>
     <BookList />
   </div>
